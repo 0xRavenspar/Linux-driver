0d7837997fe0da5d75c0e341de7f126a99752a09
0xRavenspar <ravenspar@protonmail.com>
2025-02-09 23:23:57 +0530

    Makefile changes

--- 01_hello/hello.c
+++ 01_hello/hello.c
@@ -5,3 +5,12 @@ int my_init(void){
 	printk("hello - Hello, Kernel!\n");
 	return 0;
 }
+
+void my_exit(void){
+    printk("Hello - Goodbye, Kernel\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
